94b54f5b69aeb612845b9c7e848089400f0e3032
Sheila Wittman <szandora789@gmail.com>
2018-06-25 22:33:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,8 +2,10 @@ require 'HTTParty'
 require 'Nokogiri'
 
 class Scraper
-  doc = HTTParty.get(https://heroesofthestorm.gamepedia.com/Free_rotation)
-  @parse_page ||= Nokogiri::HTML(doc)
 
+  def initialize
+    doc = HTTParty.get(https://heroesofthestorm.gamepedia.com/Free_rotation)
+    @parse_page ||= Nokogiri::HTML(doc)
+  end
 
 end
