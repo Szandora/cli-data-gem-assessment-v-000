3dbf665c890cd8bbd56485e41cd768666024d5ac
Sheila Wittman <szandora789@gmail.com>
2018-06-26 03:17:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,11 @@ require 'open-uri'
 class Scraper
 
   def get_page
-
     Nokogiri::HTML(open("https://heroesofthestorm.gamepedia.com/Free_rotation"))
-
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
-    #end
   end
+
+  def get_heroes
+   self.get_page.css(".hero-tile")
+ end
 
 end
