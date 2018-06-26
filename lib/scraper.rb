da82a78846fab4751da748bed02d7f10e1e6d465
Sheila Wittman <szandora789@gmail.com>
2018-06-26 03:33:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,13 +6,11 @@ class Scraper
 
   def get_page
     Nokogiri::HTML(open("https://heroesofthestorm.gamepedia.com/Free_rotation"))
-
-    binding.pry
   end
 
-  #def get_heroes
-   #self.get_page.css(".hero-tile")
- #end
+  def get_heroes
+   self.get_page.css(".hero-tile")
+ end
 
 end
 
